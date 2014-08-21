a3ccc37cecb3fdab52c86e252b54f94f5b0f5be5
Howon Lee <howon@howonlee.com>
2014-08-21 16:09:17 -0700

    main concourse

---
+++ main.py
@@ -0,0 +1,16 @@
+import string
+
+mongo_obj_id_settings = {
+		length: 24,
+		alphabet: string.hexdigits
+		}
+
+class BoopyBoop():
+	def __init__(self):
+		pass
+
+	def id_to_string(self, thing, list_of_words):
+		pass
+
+	def string_to_id(self, thing, list_of_words):
+		pass
