134443e4e06c4311d53de19c511961d7060cf4e8
Howon Lee <howon@howonlee.com>
2014-09-01 15:36:34 -0700

    holy shit I did not include the package in the setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # Always prefer setuptools over dis
 
 setup(
 	name='boopyboop',
-	version="0.0.1",
+	version="0.0.4",
 	description="Turn long numbers in whatever format to human-readable long series of words",
 	url="https://github.com/howonlee/boopyboop",
 	author="Howon Lee",
@@ -12,6 +12,7 @@ setup(
 		'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
 		],
-	install_requires=['python-baseconv']
+	install_requires=['python-baseconv'],
+	packages=['boopyboop']
 	)
 
